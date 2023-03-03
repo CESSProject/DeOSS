5886ebce03f3d76c7bf6b02f6af747b180496e51
AstaFrode <32032991+AstaFrode@users.noreply.github.com>
2023-03-03 18:04:56 +0800

    Commons (#22)
    * update readme
    
    * update readme and add hashtree_test.go
    
    * update readme
    
    * update reedsolomon to v1.11.7 and hashtree_test.go

--- pkg/hashtree/hashtree_test.go
+++ pkg/hashtree/hashtree_test.go
@@ -27,8 +27,13 @@ import (
 )
 
 func TestNewHashTree(t *testing.T) {
+<<<<<<< HEAD
+	var err error
+	var leafs_num int = 4
+=======
 
 	var err error
+>>>>>>> main
 	var content_one = "content_one"
 	var content_two = "content_two"
 	var content_three = "content_three"
@@ -37,7 +42,10 @@ func TestNewHashTree(t *testing.T) {
 	var content_two_hash = sha256.Sum256([]byte(content_two))
 	var content_three_hash = sha256.Sum256([]byte(content_three))
 	var content_four_hash = sha256.Sum256([]byte(content_four))
+<<<<<<< HEAD
+=======
 
+>>>>>>> main
 	var content_five = make([]byte, 0)
 	var content_sex = make([]byte, 0)
 	content_five = append(content_five, content_one_hash[:]...)
@@ -54,6 +62,18 @@ func TestNewHashTree(t *testing.T) {
 	for _, ele := range arrhashs {
 		roothashs = append(roothashs, ele)
 	}
+<<<<<<< HEAD
+
+	basedir, err := os.Getwd()
+	assert.NoError(t, err)
+
+	var file_content_one = filepath.Join(basedir, content_one)
+	var file_content_two = filepath.Join(basedir, content_two)
+	var file_content_three = filepath.Join(basedir, content_three)
+	var file_content_four = filepath.Join(basedir, content_four)
+	var chunks = []string{file_content_one, file_content_two, file_content_three, file_content_four}
+
+=======
 	var want_root_hash = hex.EncodeToString(roothashs)
 
 	basedir, err := os.Getwd()
@@ -62,6 +82,7 @@ func TestNewHashTree(t *testing.T) {
 	file_content_two := filepath.Join(basedir, content_two)
 	file_content_three := filepath.Join(basedir, content_three)
 	file_content_four := filepath.Join(basedir, content_four)
+>>>>>>> main
 	err = os.WriteFile(file_content_one, []byte(content_one), os.ModePerm)
 	assert.NoError(t, err)
 	defer os.Remove(file_content_one)
@@ -74,10 +95,26 @@ func TestNewHashTree(t *testing.T) {
 	err = os.WriteFile(file_content_four, []byte(content_four), os.ModePerm)
 	assert.NoError(t, err)
 	defer os.Remove(file_content_four)
+<<<<<<< HEAD
+
+	mtree, err := NewHashTree(chunks)
+	assert.NoError(t, err)
+
+	// Leaf hash
+	assert.Equal(t, len(mtree.Leafs), leafs_num)
+	assert.Equal(t, hex.EncodeToString(content_one_hash[:]), hex.EncodeToString(mtree.Leafs[0].Hash))
+	assert.Equal(t, hex.EncodeToString(content_two_hash[:]), hex.EncodeToString(mtree.Leafs[1].Hash))
+	assert.Equal(t, hex.EncodeToString(content_three_hash[:]), hex.EncodeToString(mtree.Leafs[2].Hash))
+	assert.Equal(t, hex.EncodeToString(content_four_hash[:]), hex.EncodeToString(mtree.Leafs[3].Hash))
+
+	// Root hash
+	assert.Equal(t, hex.EncodeToString(roothashs), hex.EncodeToString(mtree.MerkleRoot()))
+=======
 	var chunks = []string{file_content_one, file_content_two, file_content_three, file_content_four}
 
 	mtree, err := NewHashTree(chunks)
 	assert.NoError(t, err)
 	got_root_hash := hex.EncodeToString(mtree.MerkleRoot())
 	assert.Equal(t, want_root_hash, got_root_hash)
+>>>>>>> main
 }
