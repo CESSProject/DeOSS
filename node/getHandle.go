860609b62ceb55e31f786971db9c088e36c1ed9f
AstaFrode <frode@cess.one>
2023-06-19 23:55:12 +0800

    Removing padding from a file

--- node/getHandle.go
+++ node/getHandle.go
@@ -331,8 +331,13 @@ func (n *Node) fetchFiles(roothash, dir string) (string, error) {
 				if err != nil {
 					return "", err
 				}
-				f.Write(buf)
+				if (writecount + 1) >= len(fmeta.SegmentList) {
+					f.Write(buf[:(fmeta.FileSize.Uint64() - uint64(writecount*pattern.SegmentSize))])
+				} else {
+					f.Write(buf)
+				}
 				writecount++
+
 				break
 			}
 		}
