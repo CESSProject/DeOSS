2f71b566dad896e1880ef0537f1ed88fe4ebaeaf
Valor Xu <121914086+jiuquxzy@users.noreply.github.com>
2024-04-25 15:25:56 +0800

    update upload file handle,fix a bug (#143)
    * add breakpoint resume upload function
    
    * upgrade chunks upload
    
    * update upload file handle
    
    * updade deoss
    
    * add cacher and node selector config for user
    
    * clean up the code
    
    * update go mod
    
    * solve all bugs found during debugging
    
    * update save peer decorator method
    
    * add node refresh and blacklist clear method call
    
    * update upload file handle
    
    * update upload file handle
    
    ---------
    
    Co-authored-by: AstaFrode <frode@cess.one>

--- node/putHandle.go
+++ node/putHandle.go
@@ -369,10 +369,11 @@ func (n *Node) putHandle(c *gin.Context) {
 			return
 		}
 
-		if fileHeder.Size+chunksInfo.SavedFileSize > chunksInfo.TotalSize {
+		if blockNum > 0 && fileHeder.Size+chunksInfo.SavedFileSize > chunksInfo.TotalSize {
 			n.Upfile("err", fmt.Sprintf("[%v] %v", clientIp, "bad chunk size"))
 			c.JSON(http.StatusBadRequest, "bad chunk size")
 			return
+
 		}
 
 		_, err = io.Copy(f, formfile)
@@ -383,7 +384,10 @@ func (n *Node) putHandle(c *gin.Context) {
 			return
 		}
 		f.Close()
-		chunksInfo.SavedFileSize += fileHeder.Size
+
+		if blockNum > 0 {
+			chunksInfo.SavedFileSize += fileHeder.Size
+		}
 	}
 
 	if blockNum > 0 && blockIdx < blockNum-1 {
